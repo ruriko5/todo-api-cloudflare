791090b6833b9d929bb302e0adb3db73d838092d
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-02-07 20:43:56 +0900

    Changed to Chained Route

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,51 @@
 import { Hono } from 'hono';
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
 
-app.get('/todos/:id', async (c) => {
-	const id = c.req.param('id');
-	try {
-		const { results } = await c.env.DB.prepare('SELECT * FROM todos WHERE id = ?').bind(id).all();
-		return c.json(results);
-	} catch (error) {
-		return c.json({ error }, 500);
-	}
-});
-
-app.get('/todos', async (c) => {
-	try {
-		const { results } = await c.env.DB.prepare('SELECT * FROM todos').all();
-		return c.json(results);
-	} catch (error) {
-		return c.json({ error }, 500);
-	}
-});
-
-app.post('todos', async (c) => {
-	const { title, description } = await c.req.json();
-	try {
-		await c.env.DB.prepare('INSERT INTO todos (title, description) VALUES (?, ?)').bind(title, description).run();
-		return c.json({ message: 'success' }, 201);
-	} catch (error) {
-		return c.json({ error }, 500);
-	}
-});
-
-app.delete('/todos/:id', async (c) => {
-	const id = c.req.param('id');
-	try {
-		await c.env.DB.prepare('DELETE FROM todos WHERE id = ?').bind(id).run();
-		return c.json({ message: 'success' }, 200);
-	} catch (error) {
-		return c.json({ error }, 500);
-	}
-});
-
-app.put('/todos/:id', async (c) => {
-	const id = c.req.param('id');
-	const { title, description } = await c.req.json();
-	try {
-		await c.env.DB.prepare('UPDATE todos SET title = ?, description = ? WHERE id = ?').bind(title, description, id).run();
-		return c.json({ message: 'success' }, 200);
-	} catch (error) {
-		return c.json({ error }, 500);
-	}
-});
+app
+	.get('/todos', async (c) => {
+		try {
+			const { results } = await c.env.DB.prepare('SELECT * FROM todos').all();
+			return c.json(results);
+		} catch (error) {
+			return c.json({ error }, 500);
+		}
+	})
+	.get('/todos/:id', async (c) => {
+		const id = c.req.param('id');
+		try {
+			const { results } = await c.env.DB.prepare('SELECT * FROM todos WHERE id = ?').bind(id).all();
+			return c.json(results);
+		} catch (error) {
+			return c.json({ error }, 500);
+		}
+	})
+	.post('todos', async (c) => {
+		const { title, description } = await c.req.json();
+		try {
+			await c.env.DB.prepare('INSERT INTO todos (title, description) VALUES (?, ?)').bind(title, description).run();
+			return c.json({ message: 'success' }, 201);
+		} catch (error) {
+			return c.json({ error }, 500);
+		}
+	})
+	.delete('/todos/:id', async (c) => {
+		const id = c.req.param('id');
+		try {
+			await c.env.DB.prepare('DELETE FROM todos WHERE id = ?').bind(id).run();
+			return c.json({ message: 'success' }, 200);
+		} catch (error) {
+			return c.json({ error }, 500);
+		}
+	})
+	.put('/todos/:id', async (c) => {
+		const id = c.req.param('id');
+		const { title, description } = await c.req.json();
+		try {
+			await c.env.DB.prepare('UPDATE todos SET title = ?, description = ? WHERE id = ?').bind(title, description, id).run();
+			return c.json({ message: 'success' }, 200);
+		} catch (error) {
+			return c.json({ error }, 500);
+		}
+	});
 
 export default app;
