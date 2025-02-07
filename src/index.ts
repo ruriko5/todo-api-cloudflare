f3fc36d19c0e76876b8b394d11cddb7fa855f335
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-02-07 20:15:01 +0900

    get a todo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+app.get('/todos/:id', async (c) => {
+	const id = c.req.param('id');
+	try {
+		const { results } = await c.env.DB.prepare('SELECT * FROM todos WHERE id = ?').bind(id).all();
+		return c.json(results);
+	} catch (error) {
+		return c.json({ error }, 500);
+	}
+});
+
+export default app;
